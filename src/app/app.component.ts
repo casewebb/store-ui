5e9b5d04dba2e47166fa7aaf94d42edc1e539d52
Case <case.webb@gmail.com>
2020-05-29 17:38:55 -0400

    Change title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,6 @@ import { Title } from '@angular/platform-browser';
 })
 export class AppComponent {
   public constructor(private titleService: Title) {
-    this.titleService.setTitle('Things Gamers Buy');
+    this.titleService.setTitle('Cringy Gifts');
   }
 }
