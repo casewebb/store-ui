c09abade91b88261757d61ae15297a51398e246c
Case <case.webb@gmail.com>
2020-05-31 21:31:32 -0400

    Search Working

--- src/app/product-display/product-display.component.ts
+++ src/app/product-display/product-display.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../interfaces/product';
 import { ProductService } from '../services/product.service';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { combineLatest } from 'rxjs';
+import { startWith, map, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-display',
@@ -14,11 +17,23 @@ export class ProductDisplayComponent implements OnInit {
   private notScrolly: boolean = true;
   private endOfData: boolean = false;
   private nextProductPage: number = 2;
+  private searchActive: boolean = false;
 
-  constructor(private productService: ProductService) { }
+  searchForm: FormGroup;
+  get searchControls() {
+    return this.searchForm.controls;
+  }
 
-  ngOnInit(): void {
+  constructor(private productService: ProductService, private fb: FormBuilder) { }
+
+  ngOnInit() {
     this.loadInitialProducts();
+    this.searchForm = this.fb.group({
+      searchTerm: '',
+      minPrice: '',
+      maxPrice: ''
+    });
+    this.handleSearchFormChanges();
   }
 
   loadInitialProducts() {
@@ -35,7 +50,15 @@ export class ProductDisplayComponent implements OnInit {
   }
 
   loadMoreProducts() {
-    this.productService.getProducts(this.nextProductPage).subscribe(response => {
+    let upcomingProducts;
+    if (this.searchActive) {
+      upcomingProducts = this.productService.getFilteredProducts(this.nextProductPage, this.searchControls.searchTerm.value,
+        this.searchControls.maxPrice.value, this.searchControls.minPrice.value);
+    } else {
+      upcomingProducts = this.productService.getProducts(this.nextProductPage);
+    }
+
+    upcomingProducts.subscribe(response => {
       const newProducts = response['products'];
 
       if (newProducts.length == 0) {
@@ -46,7 +69,24 @@ export class ProductDisplayComponent implements OnInit {
       this.notScrolly = true;
       this.nextProductPage++;
     });
+  }
 
+  handleSearchFormChanges() {
+    this.searchForm.valueChanges
+      .pipe(
+        debounceTime(500),
+        distinctUntilChanged()
+      ).subscribe(() => {
+        this.nextProductPage = 2;
+        this.searchActive = true;
+        this.productService.getFilteredProducts(1, this.searchControls.searchTerm.value,
+          this.searchControls.maxPrice.value, this.searchControls.minPrice.value).subscribe(response => {
+            this.products = response['products']
+            if (this.products.length > 0) {
+              this.endOfData = false;
+            }
+          });
+      })
   }
 
 
